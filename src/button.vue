1fa5723d74f06f1c5017271626b7678ae41520f6
wangtao <1182792703@qq.com>
2021-06-29 23:10:08 +0800

    添加icon和icon position 属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,16 @@
 <template>
-  <button class="g-button">hi</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
   export  default {
-
+    props: ['icon', 'iconPosition']
   }
 </script>
 <style lang="scss">
@@ -15,7 +22,10 @@
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover{
     border-color: var(--border-color-hover);
   }
@@ -24,6 +34,23 @@
   }
   &:focus{
     outline: none;
+  }
+  > .icon{
+    order: 1;
+    margin-right: .3em;
+  }
+  > .content{
+    order: 2;
+  }
+  &.icon-right{
+    > .icon{
+      order: 2;
+      margin-left: .3em;
+      margin-right: 0;
+    }
+    > .content{
+      order: 1;
+    }
   }
 }
 </style>
