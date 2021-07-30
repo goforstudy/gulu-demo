c7698b72a5e857a97a69259f5a36087caf30ac68
wangtao <1182792703@qq.com>
2021-07-30 23:28:44 +0800

    添加gutter 和 offset

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
-        <slot/>
+    <div :class="colClass" :style="colStyle">
+      <slot></slot>
     </div>
 </template>
 <script>
@@ -9,20 +9,44 @@ export default {
     props: {
         span: {
             type: [Number, String],
+        },
+        offset: {
+            type: [Number, String],
+        }
+    },
+    data() {
+        return {
+            gutter: 0
+        }
+    },
+    computed: {
+        colClass() {
+            const {span, offset} = this
+            return {col: true,[`col-${span}`]: span, [`offset-${offset}`]:offset}
+        },
+        colStyle() {
+            const {gutter} = this
+            return {
+                        paddingLeft: `${gutter/2}px`,
+                        paddingRight: `${gutter/2}px`
+                    }
         }
     }
 }
 </script>
 <style scoped lang="scss">
     .col{
-        height: 100px;
-        background: grey;
         width: 50%;
-        border: 1px solid red;
         $class: col-;
         @for $n from 1 through 24{
             &.#{$class}#{$n}{
                 width: ($n/24)*100%;
+            }
+        }
+        $class: offset-;
+        @for $n from 1 through 24{
+            &.#{$class}#{$n}{
+                margin-left: ($n/24)*100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,30 @@
 <template>
-    <div class="row">
+    <div class="row" :style="rowStyle">
         <slot/>
     </div>
 </template>
+<script>
+export default {
+    name: 'GuluRow',
+    props: {
+        gutter: {
+            type: [Number, String],
+            default: 0
+        }
+    },
+    computed: {
+        rowStyle() {
+            const {gutter} = this
+            return {marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}
+        }
+    },
+    mounted() {
+        this.$children.forEach((vm) => {
+            vm.gutter = this.gutter;
+        })
+    }
+}
+</script>
 <style scoped lang="scss">
     .row{
         display: flex;
