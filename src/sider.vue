c1579065302705096466229530c87bdb9d7c270d
wangtao <1182792703@qq.com>
2021-08-02 21:30:21 +0800

    完成sider关闭功能

--- src/sider.vue
+++ src/sider.vue
@@ -1,14 +1,37 @@
 <template>
-    <div class="sider">
-        <slot></slot>
-    </div>
+    <transition name="side">
+        <div class="sider" v-if="visible">
+            <slot></slot>
+            <button @click="visible=false">close</button>
+        </div>
+    </transition>
 </template>
 <script>
 export default {
-    name: 'GuluSider'
+    name: 'GuluSider',
+    data() {
+        return {
+            visible: true
+        };
+    },
+    methods: {
+
+    }
 }
 </script>
 <style sider lang="scss">
     .sider{
+        position: relative;
+        > button {
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+    }
+    .side-enter-active, .side-leave-active {
+        transition: all .5s;
+    }
+    .side-enter, .side-leave-to{
+        margin-left: -200px;
     }
 </style>
