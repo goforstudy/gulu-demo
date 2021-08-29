57a8f57971eed22624442fe2812d0b0508d90583
wangtao <1182792703@qq.com>
2021-08-29 09:46:47 +0800

    完成基本的collapse

---
+++ src/collapse.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+export default {
+    name: 'GuluCollapse',
+    props: {
+        single: {
+            type: Boolean,
+            default: false
+        },
+        selected: {
+            type: String
+        }
+    },
+    data() {
+        return {
+            eventBus: new Vue()
+        }
+    },
+    provide() {
+        return {
+            eventBus: this.single ? null : this.eventBus
+        }
+    },
+    mounted() {
+        this.eventBus.$emit('update:selected', this.selected)
+        this.eventBus.$on('update:selected', (name) => {
+            this.$emit('update:selected', name)
+        })
+    }
+}
+</script>
+<style lang="scss" scoped>
+    $grey: #ddd;
+    $border-radius: 4px;
+    .collapse {
+        border: 1px solid $grey;
+        border-radius: $border-radius;
+    }
+</style>
